1fd4832434e27c0df20b77ce123a5ae9c5d40218
Franke <frankedeveloper@gmail.com>
2022-06-21 12:45:57 -0300

    refactor: remove mysql artisan migrate

--- docker/mysql/create-database.sh
+++ docker/mysql/create-database.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-mysql --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
+mysql --user=root --password="password" <<-EOSQL
     CREATE DATABASE IF NOT EXISTS dacxi_database;
     GRANT ALL PRIVILEGES ON \`dacxi_database%\`.* TO '$MYSQL_USER'@'%';
 EOSQL
-
-./vendor/bin/sail artisan migrate
 
